bfef7de2a5735c822b27a75cbbbfd3feb3a11041
LincolnK88 <Thelinkster88@gmail.com>
2025-03-06 16:47:08 -0600

    Update pipes.py

--- pipes.py
+++ pipes.py
@@ -1,11 +1,21 @@
 import pygame
 
 class Pipe:
-    def __init__(self, pos, img):
+    def __init__(self, pos, img, speed):
         self.pos = list(pos)
         self.img = img
+        self.speed = speed
+    def update(self):
+        self.pos[0] += self.speed
 
+    def render(self):
+        # render a pipe twice with different orientations
+        pass
+        
 class Pipes:
     def __init__(self, speed):
-        self.pipes = []
-        self.speed = speed
+        pass
+    def render(self):
+        pipe.render() 
+
+    
