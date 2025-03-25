8b962c19e48a8c7f015d0426f68550272c72c407
The-Ghost-King <smae641129@icloud.com>
2025-03-25 16:26:15 -0500

    Update entities.py

--- entities.py
+++ entities.py
@@ -4,11 +4,16 @@ class Bird:
     def __init__(self, game, pos, size):
         self.game = game
         self.pos = list(pos)
-        self.size = size
+        self.size = (int(size[0] * 2), int(size[1] * 2))
         self.velocity = 0
         self.gravity = 0.5
         self.jump_strength = -8
         self.offscreen = False
+        self.image = pygame.image.load("bird image.png")
+        self.image = pygame.transform.scale(self.image, self.size)
+
+    def render(self, surf):
+        surf.blit(self.image, (self.pos[0], self.pos[1]))
 
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
@@ -16,22 +21,18 @@ class Bird:
     def update(self):
         self.velocity += self.gravity
         self.pos[1] += self.velocity
-    if self.pos[1] <= 0:
-        self.pos[1] = 0
-        self.velocity = 0
-        self.game_over() 
-        def game_over(self):
+        if self.pos[1] <= 0:
+            self.pos[1] = 0
+            self.velocity = 0
+            self.game_over() 
+    def game_over(self):
         print("Game Over! You died")
         self.is_game_over = True
-        # check if offscreen
         rect = self.rect()
         if (rect.right < 0 or rect.left > self.game.SCREEN_WIDTH or
-            rect.bottom < 0 or rect.top > self.game.SCREEN_HEIGHT):
+        rect.bottom < 0 or rect.top > self.game.SCREEN_HEIGHT):
             self.offscreen = True
         print("Offscreen = ", self.offscreen)
-
-    def render(self, surf):
-        pygame.draw.rect(surf, (255, 255, 0), (self.pos[0], self.pos[1], self.size[0], self.size[1]))
 
 
 class Player(Bird):
@@ -39,6 +40,6 @@ class Player(Bird):
         super().__init__(game, pos, size)
         self.jumps = True 
         self.score = 0
-    
+
     def jump(self):
         self.velocity = self.jump_strength
